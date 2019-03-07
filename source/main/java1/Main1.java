42041e088c3e2416f1a8808271d1dcc06789f9fc
adityattn <aditya.kumar1@tothenew.com>
2019-03-07 11:03:03 +0530

    gradle project

---
+++ source/main/java1/Main1.java
@@ -0,0 +1,6 @@
+class Main1{
+    public Main1()
+    {
+        System.out.println("Parent main1");;
+    }
+}
---
+++ source/main/java2/Main2.java
@@ -0,0 +1,9 @@
+class Main2 extends Main1{
+    public Main2()
+    {
+        System.out.println("Child Main2");
+    }
+    public static void main(String[] arg){
+        Main2 main2=new Main2();
+    }
+}
